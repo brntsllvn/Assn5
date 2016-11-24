6686ff2b308a6acf39dd3976d037ff60f389f9f8
Brent Sullivan <brntsllvn@gmail.com>
2016-11-24 11:51:43 +1100

    update shopping

--- src/main/scala/com/persist/uw/examples/RecipeClasses.scala
+++ src/main/scala/com/persist/uw/examples/RecipeClasses.scala
@@ -4,28 +4,90 @@ object RecipeClasses {
 
   //  def DollarsCents(dollars: Int = 0, cents: Int = 0) = Cost
   //
-  //  case class Cost(cents: Int) {
-  //    def *(i: Int) = Cost(i * cents)
-  //    def /(i: Int) = Cost((cents + i - 1) / i)
-  //    private def dollarSign = "$"
-  //      override def toString: String = f"$dollarSign${cents / 100}.${cents % 100}%02d"
-  //
-  //  }
+
+  case class Cost(cents: Int) {
+    def *(i: Int) = Cost(i * cents)
+
+    def /(i: Int) = Cost((cents + i - 1) / i)
+
+    private def dollarSign = "$"
+
+    override def toString: String = f"$dollarSign${cents / 100}.${cents % 100}%02d"
+
+  }
+
+  case class HomeInventoryIngredient(rawIngredient: RawIngredient, inventory: Int) {}
+
+  case class HomePantry(inventory: List[HomeInventoryIngredient]) {
+
+    def GetInventory: List[String] = {
+      def GetInventoryWithAccum(inventory: List[HomeInventoryIngredient], accum: List[String]): List[String] = {
+        inventory match {
+          case firstItemInInventory :: remainingItemsInInventory => {
+            GetInventoryWithAccum(remainingItemsInInventory,
+              (firstItemInInventory.inventory.toString
+                + " "
+                + firstItemInInventory.rawIngredient.unitOfMeasurement.toString
+                + " "
+                + firstItemInInventory.rawIngredient.name.toString)
+                :: accum)
+          }
+          case Nil => accum
+        }
+      }
+      GetInventoryWithAccum(inventory, Nil)
+    }
+
+    def GoShoppingFor(recipes: List[Recipe]): List[String] = {
+      def GoShoppingWithAccum(recipes: List[Recipe], accum: List[List[String]]): List[List[String]] = {
+        recipes match {
+          case firstRecipe :: remainingRecipes => {
+            GoShoppingWithAccum(remainingRecipes, firstRecipe.FormatRawIngredients :: accum)
+          }
+          case Nil => accum
+        }
+      }
+//      def SpecialFlatten(ls: List[Any]): List[Any] = ls flatMap {
+//        case i: List[_] => SpecialFlatten(i)
+//        case e => List(e)
+//      }
+//      SpecialFlatten(GoShoppingWithAccum(recipes, Nil))
+      GoShoppingWithAccum(recipes, Nil).flatten
+    }
+
+  }
+
 
   case class RawIngredient(
                             val name: String,
                             val unitOfMeasurement: String,
                             val numberOfUnitsInContainer: Int,
                             val pricePerContainer: Int
-                          )
+                          ) {
+    def format: String = {
+      (this.numberOfUnitsInContainer +
+        " " +
+        this.unitOfMeasurement +
+        " " +
+        this.name)
+    }
+  }
 
   case class RecipeIngredient(
-                              val recipe: String,
-                              val rawIngredient: RawIngredient,
-                              val numberOfUnits: Int
-                             )
+                               val recipe: String,
+                               val rawIngredient: RawIngredient,
+                               val numberOfUnits: Int
+                             ) {
+    def format: String = {
+      (this.numberOfUnits.toString +
+        " " +
+        this.rawIngredient.unitOfMeasurement.toString +
+        " " +
+        this.rawIngredient.name.toString)
+    }
+  }
 
-  class Recipe(ingredients: List[RecipeIngredient]) {
+  case class Recipe(ingredients: List[RecipeIngredient]) {
 
     def CalculateRecipeCost: Int = {
       def CalculateRecipeCostWithAccum(ingredients: List[RecipeIngredient], accum: Int): Int = {
@@ -37,6 +99,30 @@ object RecipeClasses {
         }
       }
       CalculateRecipeCostWithAccum(ingredients, 0)
+    }
+
+    def FormatRecipeIngredients: List[String] = {
+      def FormatIngredientsWithAccum(ingredients: List[RecipeIngredient], accum: List[String]): List[String] = {
+        ingredients match {
+          case firstIngredient :: remainingIngredients => {
+            FormatIngredientsWithAccum(remainingIngredients, firstIngredient.format :: accum)
+          }
+          case Nil => accum
+        }
+      }
+      FormatIngredientsWithAccum(ingredients, Nil)
+    }
+
+    def FormatRawIngredients: List[String] = {
+      def FormatRawIngredientsWithAccum(ingredients: List[RawIngredient], accum: List[String]): List[String] = {
+        ingredients match {
+          case firstIngredient :: remainingIngredients => {
+            FormatRawIngredientsWithAccum(remainingIngredients, firstIngredient.format :: accum)
+          }
+          case Nil => accum
+        }
+      }
+      FormatRawIngredientsWithAccum(ingredients.map(_.rawIngredient), Nil)
     }
   }
 
--- src/test/scala/LambdaTest/TestRecipes.scala
+++ src/test/scala/LambdaTest/TestRecipes.scala
@@ -12,86 +12,152 @@ class TestRecipes extends LambdaTest {
 
   def nop: Unit = ()
 
-  val r = RecipeExample
+  import RecipeExample._
 
-  import r._
+  def act = {
+    val rawTomato = RawIngredient("tomato", "tomato", 1, 200)
+    val rawBacon = RawIngredient("bacon", "strip", 30, 400)
+    val rawBread = RawIngredient("bread", "slice", 20, 200)
+    val rawLettuce = RawIngredient("lettuce", "leaf", 30, 200)
+    val rawSwissCheese = RawIngredient("swiss cheese", "slice", 20, 200)
+    val rawEggs = RawIngredient("eggs", "egg", 12, 300)
+    val rawCream = RawIngredient("cream", "T", 10, 200)
+    val rawSalt = RawIngredient("salt", "pinch", 100, 100)
+    val rawPepper = RawIngredient("pepper", "pinch", 100, 1)
+    val rawButter = RawIngredient("butter", "T", 12, 200)
 
-  def act =
+    val bltTomato = RecipeIngredient("blt", rawTomato, 1)
+    val bltBacon = RecipeIngredient("blt", rawBacon, 3)
+    val bltBread = RecipeIngredient("blt", rawBread, 2)
+    val bltLettuce = RecipeIngredient("lettuce", rawLettuce, 2)
+    val bltIngredients = List(bltTomato, bltBacon, bltBread, bltLettuce)
+
+    val seEggs = RecipeIngredient("scrambled eggs", rawEggs, 3)
+    val seSalt = RecipeIngredient("scrambled eggs", rawSalt, 1)
+    val sePepper = RecipeIngredient("scrambled eggs", rawPepper, 1)
+    val seButter = RecipeIngredient("scrambled eggs", rawButter, 1)
+    val scrambledEggsIngredients = List(seButter, sePepper, seSalt, seEggs)
+    val scrambledEggs = Recipe(scrambledEggsIngredients)
+
+    val quicheCream = RecipeIngredient("quiche", rawCream, 10)
+    val quicheEgg = RecipeIngredient("quiche", rawEggs, 3)
+    val quicheSwissCheese = RecipeIngredient("quiche", rawSwissCheese, 2)
+    val quicheBacon = RecipeIngredient("quiche", rawBacon, 3)
+    val quicheIngredients = List(quicheCream, quicheSwissCheese, quicheBacon, quicheEgg)
+    val quiche = Recipe(quicheIngredients)
+
+    test("how does flatten work?"){
+      val flatStuffs = List(
+        List("hi stuff", "bye"),
+        List("bye", "eh?")
+      ).flatten
+
+      assertEq(flatStuffs,List("hi stuff","bye","bye","eh?"))
+    } +
+    test("how about flatten with complex stuffs"){
+      val flatRawIngredients = List(
+        List(rawBacon, rawBread),
+        List(rawButter, rawCream),
+        List(rawSalt, rawPepper)
+      ).flatten
+
+      assertEq(flatRawIngredients, List(rawBacon, rawBread, rawButter, rawCream, rawSalt, rawPepper))
+    } +
+    test("does nested list equality work?") {
+      assertEq(List(List(1,2)),List(List(1,2)))
+    } +
     test("What is the cost of the ingredients in a blt?") {
-      val rawTomato = RawIngredient("tomato", "tomato", 1, 2)
-      val bltTomato = RecipeIngredient("blt", rawTomato, 1)
-
-      val rawBacon = RawIngredient("bacon", "slice", 30, 4)
-      val bltBacon = RecipeIngredient("blt", rawBacon, 3)
-
-      val rawBread = RawIngredient("bread", "slice", 20, 2)
-      val bltBread = RecipeIngredient("blt", rawBread, 2)
-
-      val rawLettuce = RawIngredient("lettuce", "leaf", 30, 2)
-      val bltLettuce = RecipeIngredient("lettuce", rawLettuce, 2)
-
-      val bltIngredients = List(bltTomato, bltBacon, bltBread, bltLettuce)
-
       val expectedBltCost = (bltTomato.rawIngredient.pricePerContainer
         + bltBacon.rawIngredient.pricePerContainer
         + bltBread.rawIngredient.pricePerContainer
         + bltLettuce.rawIngredient.pricePerContainer)
+      val formattedExpectedBltCost = Cost(expectedBltCost).toString
 
-      val blt = new Recipe(bltIngredients)
+      val blt = Recipe(bltIngredients)
       val actualBltCost = blt.CalculateRecipeCost
+      val formattedActualBltCost = Cost(actualBltCost).toString
 
-      assertEq(actualBltCost, expectedBltCost, "blt costs")
-
-
+      assertEq(formattedActualBltCost, formattedExpectedBltCost, "blt costs")
       // assertEq(cost,cost,"blt costs")
-      //      nyi
-    }
+    } +
+      test("What ingredients do I need to make a quiche?") {
+        val formattedQuicheIngredients = quiche.FormatRecipeIngredients
 
-//      test("What ingredients do I need to make a quiche?") {
-//        nyi
-//      } +
-//      test("What ingredients do I have in my house?") {
-//        nyi
-//      } +
-//      test("Buying everything I need to make scrambled eggs and a quiche") {
-//        // this is in addition to what is already in house
-//        // Note: you can only buy whole packages (so round up)
-//        // Report the cost of groceries purchased
-//        nyi
-//      } +
-//      test("What ingredients do I have in my house?") {
-//        nyi
-//      } +
-//      label("Preparing a quiche") {
-//        exec {
-//          // this should remove the ingredients used from the inventory
-//          nop
-//        }
-//      } +
-//      test("What ingredients do I have in my house?") {
-//        // After the quiche wa prepared
-//        nyi
-//      } +
-//      test("What recipes in my cookbook use bacon?") {
-//        nyi
-//      } +
-//      test("How do I prepare a blt?") {
-//        nyi
-//      } +
-//      test("What recipes in my cookbook do I now have enough ingredients to prepare?") {
-//        nyi
-//      } +
-//      test("Going shopping") {
-//        // Set up shopping list with 10 tomatoes and 50 T butter
-//        // Only whole packages can be purchased
-//        // Add my purchases to my inventory
-//        // Report the cost of the purchase
-//        nyi
-//      } +
-//      test("What ingredients do I have in my house?") {
-//        // After shopping
-//        nyi
-//      }
+        val expectedQuicheIngredients = List("3 egg eggs", "3 strip bacon", "2 slice swiss cheese", "10 T cream")
+
+        assertEq(formattedQuicheIngredients, expectedQuicheIngredients, "quiche ingredients")
+      } +
+      test("What ingredients do I need to make scrambled eggs?") {
+        val expectedScrambledEggsIngredients = List("3 egg eggs", "1 pinch salt", "1 pinch pepper", "1 T butter")
+        val formattedScrambledEggsIngredients = scrambledEggs.FormatRecipeIngredients
+
+        assertEq(formattedScrambledEggsIngredients, expectedScrambledEggsIngredients, "scrambled eggs ingredients")
+      } +
+      test("What ingredients do I have in my house?") {
+        val homePantry = HomePantry(List())
+        val actualIngredientsInHome = homePantry.GetInventory
+
+        val expectedIngredientsInHome = List()
+
+        assertEq(actualIngredientsInHome, expectedIngredientsInHome, "in my house")
+      } +
+      test("Buying everything I need to make scrambled eggs") {
+        val homePantry = HomePantry(List())
+        val homePantryAfterShopping = homePantry.GoShoppingFor(List(scrambledEggs))
+
+        val expectedShoppingList = scrambledEggs.FormatRawIngredients
+
+        assertEq(homePantryAfterShopping, expectedShoppingList, "scrambled eggs now in inventory")
+      } +
+      test("Buying everything I need to make scrambled eggs and a quiche") {
+        val homePantry = HomePantry(List())
+        val homePantryAfterShopping = homePantry.GoShoppingFor(List(scrambledEggs, quiche))
+
+        val se = scrambledEggs.FormatRawIngredients
+        val qu = quiche.FormatRawIngredients
+        val expectedShoppingList = List(se,qu)
+
+        assertEq(homePantryAfterShopping, expectedShoppingList, "scrambled eggs and quiche now in inventory")
+        // this is in addition to what is already in house
+        // Note: you can only buy whole packages (so round up)
+        // Report the cost of groceries purchased
+
+      }
+    // +
+    //      test("What ingredients do I have in my house?") {
+    //        nyi
+    //      } +
+    //      label("Preparing a quiche") {
+    //        exec {
+    //          // this should remove the ingredients used from the inventory
+    //          nop
+    //        }
+    //      } +
+    //      test("What ingredients do I have in my house?") {
+    //        // After the quiche wa prepared
+    //        nyi
+    //      } +
+    //      test("What recipes in my cookbook use bacon?") {
+    //        nyi
+    //      } +
+    //      test("How do I prepare a blt?") {
+    //        nyi
+    //      } +
+    //      test("What recipes in my cookbook do I now have enough ingredients to prepare?") {
+    //        nyi
+    //      } +
+    //      test("Going shopping") {
+    //        // Set up shopping list with 10 tomatoes and 50 T butter
+    //        // Only whole packages can be purchased
+    //        // Add my purchases to my inventory
+    //        // Report the cost of the purchase
+    //        nyi
+    //      } +
+    //      test("What ingredients do I have in my house?") {
+    //        // After shopping
+    //        nyi
+    //      }
+  }
 }
 
 
